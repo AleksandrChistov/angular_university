8f1300bdc563876febb8e29925a9b0634c3ebd29
Alexander <aleksandrchistov.ru@yandex.ru>
2021-08-21 20:17:58 +0700

    added the folder 2 part

---
+++ 2 part/server.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import {Application} from 'express/lib/router';
+import {getAllCourses} from './server/get-courses.route';
+import {saveCourse} from './server/save-course.route';
+
+const bodyParser = require('body-parser');
+
+console.log(bodyParser);
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+app.route('/api/courses').get(getAllCourses);
+
+app.route('/api/courses/:id').put(saveCourse);
+
+const httpServer = app.listen(9000, () => {
+    console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
+});
